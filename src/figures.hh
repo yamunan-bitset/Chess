b58ae726f0fc85b9df37424bd6c5200da0282edc
yg-cpu <anirudhyamunang@gmail.com>
2021-06-04 19:48:16 +0200

    started oop project

--- src/figures.hh
+++ src/figures.hh
@@ -1,48 +1,23 @@
-// Black Textures
-sf::Texture b_pawn_t;
-sf::Texture b_king_t;
-sf::Texture b_queen_t;
-sf::Texture b_rook_t;
-sf::Texture b_knight_t;
-sf::Texture b_bishop_t;
-// White Textures
-sf::Texture w_pawn_t;
-sf::Texture w_king_t;
-sf::Texture w_queen_t;
-sf::Texture w_rook_t;
-sf::Texture w_knight_t;
-sf::Texture w_bishop_t;
-
 // Loading File for Textures Black
-b_pawn_t.loadFromFile("Assets/b_pawn.png");
-b_king_t.loadFromFile("Assets/b_king.png");
-b_queen_t.loadFromFile("Assets/b_queen.png");
-b_rook_t.loadFromFile("Assets/b_rook.png");
-b_knight_t.loadFromFile("Assets/b_knight.png");
-b_bishop_t.loadFromFile("Assets/b_bishop.png");
+std::string b_pawn   = "Assets/b_pawn.png";
+std::string b_king   = "Assets/b_king.png";
+std::string b_queen  = "Assets/b_queen.png";
+std::string b_rook   = "Assets/b_rook.png";
+std::string b_knight = "Assets/b_knight.png";
+std::string b_bishop = "Assets/b_bishop.png";
 // Loading File for Textures White  
-w_pawn_t.loadFromFile("Assets/w_pawn.png");
-w_king_t.loadFromFile("Assets/w_king.png");
-w_queen_t.loadFromFile("Assets/w_queen.png");
-w_rook_t.loadFromFile("Assets/w_rook.png");
-w_knight_t.loadFromFile("Assets/w_knight.png");
-w_bishop_t.loadFromFile("Assets/w_bishop.png");
-  
-// Black Sprites
-sf::Sprite b_pawn(b_pawn_t);
-sf::Sprite b_king(b_king_t);
-sf::Sprite b_queen(b_queen_t);
-sf::Sprite b_rook(b_rook_t);
-sf::Sprite b_knight(b_knight_t);
-sf::Sprite b_bishop(b_bishop_t);
-// White Sprites
-sf::Sprite w_pawn(w_pawn_t);
-sf::Sprite w_king(w_king_t);
-sf::Sprite w_queen(w_queen_t);
-sf::Sprite w_rook(w_rook_t);
-sf::Sprite w_knight(w_knight_t);
-sf::Sprite w_bishop(w_bishop_t);
+std::string w_pawn   = "Assets/w_pawn.png";
+std::string w_king   = "Assets/w_king.png";
+std::string w_queen  = "Assets/w_queen.png";
+std::string w_rook   = "Assets/w_rook.png";
+std::string w_knight = "Assets/w_knight.png";
+std::string w_bishop = "Assets/w_bishop.png";
 
-std::array<sf::Sprite, 6> b_sprites = { b_pawn, b_king, b_queen, b_rook, b_knight, b_bishop };
-std::array<sf::Sprite, 6> w_sprites = { w_pawn, w_king, w_queen, w_rook, w_knight, w_bishop };
+std::array<std::string, 6> b_sprites = { b_pawn, b_king, b_queen, b_rook, b_knight, b_bishop };
+std::array<std::string, 6> w_sprites = { w_pawn, w_king, w_queen, w_rook, w_knight, w_bishop };
+std::vector<ChessPiece> pieces;
 
+for (unsigned int i = 0; i < w_sprites.size(); i++)
+  pieces.push_back(ChessPiece(window, w_sprites[i]));
+for (unsigned int i = 0; i < b_sprites.size(); i++)
+  pieces.push_back(ChessPiece(window, b_sprites[i]));
