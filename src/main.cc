97014509e9e2e93db5e7142163a334c75ef9a0dd
yg-cpu <anirudhyamunang@gmail.com>
2021-06-06 11:08:08 +0200

    TODO: Resize png's

--- src/main.cc
+++ src/main.cc
@@ -2,11 +2,10 @@
 #include <string>
 #include <array>
 #include <vector>
-#include <iostream>
 
 #include "chess_piece.hh"  
 
-int main()
+int main(int argc, char** argv)
 {
   sf::RenderWindow window(sf::VideoMode(1000, 1000),"Chess");
 
@@ -14,9 +13,7 @@ int main()
 
   for (int i = 0; i < pieces.size(); i++)
     {
-      /*pieces[i].setOrigin(sf::Vector2f(pieces[i].getTexture()->getSize().x/2,
-			  pieces[i].getTexture()->getSize().y/2));
-      pieces[i].setScale(sf::Vector2f(0.5f, 0.5f));*/
+      pieces[i].setScale(sf::Vector2f(1.0f, 1.0f));
       pieces[i].setPosition(sf::Vector2f(100*(i % 8) + 50, 
  				        (i < 8 ? 50:(i < 16 ? 150:(i < 24 ? 650:750)))));
     }
@@ -30,10 +27,11 @@ int main()
 	  window.close();
       for (unsigned int i = 0; i < pieces.size(); i++)
 	pieces[i].move(moving_piece);
+
       window.clear();
       for (unsigned int i = 0; i < pieces.size(); i++)
 	window.draw(pieces[i]);
 
-	window.display();
+      window.display();
     }
 }
